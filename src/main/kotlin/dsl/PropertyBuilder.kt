1c2a4489e4b6c73e19921e4e694cf1d932e16b5d
BuildTools <DeoTimeTheGithubUser@users.noreply.github.com>
2022-12-07 12:45:37 -0600

    Properties

---
+++ src/main/kotlin/dsl/PropertyBuilder.kt
@@ -0,0 +1,38 @@
+package dsl
+
+import com.squareup.kotlinpoet.AnnotationSpec
+import com.squareup.kotlinpoet.PropertySpec
+import com.squareup.kotlinpoet.jvm.transient
+import com.squareup.kotlinpoet.jvm.volatile
+import dsl.utils.Assembler
+import dsl.utils.Cozy
+import dsl.utils.buildWith
+import dsl.utils.withRequired
+
+class PropertyBuilder(private val cozy: Cozy<PropertyBuilder> = Cozy()) :
+    Attributes.Cozied<PropertyBuilder>(cozy),
+    Attributes.Sourced<PropertySpec.Builder>,
+    Attributes.Buildable<PropertySpec> by Attributes.buildWith(cozy, PropertySpec.Builder::build),
+    Attributes.Has.Type by Attributes.typedHolder(cozy),
+    Attributes.Has.Type.Parameters by Attributes.parameterizedTypeVisitor(cozy, PropertySpec.Builder::typeVariables),
+    Attributes.Property by Attributes.property(
+        cozy = cozy,
+        modifiers = PropertySpec.Builder::modifiers,
+        annotations = PropertySpec.Builder::annotations
+    ) {
+
+    override val source by withRequired { PropertySpec.builder(name, type) }
+
+    fun mutable() { source.mutable(true) }
+    fun immutable() { source.mutable(false) }
+    fun transient() { source.transient() }
+    fun volatile() { source.volatile() }
+
+    fun initializer(assembler: Assembler<CodeBuilder>) { source.initializer(CodeBuilder().buildWith(assembler)) }
+    fun getter(assembler: Assembler<FunctionBuilder>) {
+        source.getter(FunctionBuilder().apply { getter() }.buildWith(assembler))
+    }
+    fun setter(assembler: Assembler<FunctionBuilder>) {
+        source.setter(FunctionBuilder().apply { setter() }.buildWith(assembler))
+    }
+}
--- src/main/kotlin/dsl/utils/Cozy.kt
+++ src/main/kotlin/dsl/utils/Cozy.kt
@@ -6,6 +6,5 @@ import kotlin.reflect.KProperty
 class Cozy<T> {
     private var value: Any? = null
     operator fun getValue(ref: Any?, prop: KProperty<*>?) = value!! as T
-
     operator fun invoke(ref: Any) { value = ref }
 }
