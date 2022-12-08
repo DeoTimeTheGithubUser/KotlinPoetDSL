f7b25585ce94e836f456512348d0a6971c2a8eb0
BuildTools <DeoTimeTheGithubUser@users.noreply.github.com>
2022-12-08 15:43:03 -0600

    Fix

--- src/main/kotlin/dsl/Cozy.kt
+++ src/main/kotlin/dsl/Cozy.kt
@@ -3,11 +3,9 @@ package dsl
 import kotlin.reflect.KProperty
 
 class Cozy<T> {
-    private var value: Any? = null
-    operator fun getValue(ref: Any?, prop: KProperty<*>?) = value!! as T
-    operator fun invoke(ref: Any?) {
-        value = ref
-    }
+    private var value: T? = null
+    operator fun getValue(ref: Any?, prop: KProperty<*>?) = value
+    operator fun invoke(ref: T) { value = ref }
 
     abstract class Initializer<T>(private val initializer: (Cozy<T>) -> T) {
         fun cozy() = Cozy<T>().let { cozy -> initializer(cozy).also { cozy(it) } }
