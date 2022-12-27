4d7fb50798e7597e2caac27c9bf7f3a74a9f34a1
deotime <89555032+DeoTimeTheGithubUser@users.noreply.github.com>
2022-12-27 17:43:46 -0600

    Fix

--- src/main/kotlin/me/deotime/kpoetdsl/ksp/kspTypes.kt
+++ src/main/kotlin/me/deotime/kpoetdsl/ksp/kspTypes.kt
@@ -5,15 +5,18 @@ import com.google.devtools.ksp.symbol.KSType
 import com.google.devtools.ksp.symbol.KSTypeArgument
 import com.google.devtools.ksp.symbol.Variance
 import com.squareup.kotlinpoet.KModifier
+import com.squareup.kotlinpoet.ParameterizedTypeName
 import com.squareup.kotlinpoet.ParameterizedTypeName.Companion.parameterizedBy
 import com.squareup.kotlinpoet.STAR
 import com.squareup.kotlinpoet.TypeName
 import com.squareup.kotlinpoet.TypeVariableName
 
 @KspExperimental
-fun KSType.asTypeName(): TypeName = declaration.className.parameterizedBy(
-    arguments.map { it.asTypeVariableName() }
-).copy(nullable = isMarkedNullable)
+fun KSType.asTypeName(): TypeName = declaration.className.let {
+    if(arguments.isNotEmpty()) it.parameterizedBy(
+        arguments.map { it.asTypeVariableName() }
+    ) else it
+} .copy(nullable = isMarkedNullable)
 
 @KspExperimental
 fun KSTypeArgument.asTypeVariableName(): TypeName =
