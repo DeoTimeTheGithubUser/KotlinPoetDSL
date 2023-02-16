427834c842ff416dbea95ff9bab2d1183ada4b89
BuildTools <DeoTimeTheGithubUser@users.noreply.github.com>
2023-02-16 09:50:57 -0600

    Fix vararg in metadata

--- metadata/src/main/kotlin/me/deotime/kpoetdsl/metadata/metaParameters.kt
+++ metadata/src/main/kotlin/me/deotime/kpoetdsl/metadata/metaParameters.kt
@@ -1,6 +1,7 @@
 package me.deotime.kpoetdsl.metadata
 
 import com.squareup.kotlinpoet.KModifier
+import com.squareup.kotlinpoet.ParameterizedTypeName
 import kotlinx.metadata.KmValueParameter
 import me.deotime.kpoetdsl.Cozy.Initializer.Simple.Companion.invoke
 import me.deotime.kpoetdsl.ExperimentalKotlinPoetDSL
@@ -12,7 +13,11 @@ fun KmValueParameter.toSpec() = let { km ->
         type(km.type.asTypeName())
         modifiers {
             +km.flags.toParameterModifiers()
-            km.varargElementType?.let { +KModifier.VARARG }
+            km.varargElementType?.let {
+                +KModifier.VARARG
+                // use the array type
+                type((type as ParameterizedTypeName).typeArguments.first())
+            }
         }
     }
 }
